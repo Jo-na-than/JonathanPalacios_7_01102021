452d5b7ebbc329372324f770dd74eabbb2e95419
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:53:12 +0200

    enregistrement new user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -49,3 +49,78 @@ function decrypt(text){
     dec += decipher.final('utf8');
     return dec;
 }
+
+// Créer une route pour enregistrer nouvel utilisateur
+exports.signup = ((req, res) => {
+    const userData = req.body;
+    console.log(userData)       // OK
+
+    // Crypter email entrée
+    let emailHash = encrypt(userData.email)
+    console.log(emailHash + " email")
+
+        // Valider les données du email, nom, prénom, fonction avec validator
+    if( !validator.isEmail(userData.email)) {return res.status(400).json({message: " Email invalid"})}
+
+    if(!validator.matches(userData.nom, /^[a-zéèàùûêâôë][a-zéèàùûêâôë '-]+$/i)) {return res.status(400).json({message: " Nom ne peut être que les lettres"})}
+
+    if (!validator.matches(userData.prenom, /^[a-zéèàùûêâôë][a-zéèàùûêâôë '-]+$/i)) {return res.status(400).json({message: " Prenom ne peut être que les lettres"})}
+
+    if (!validator.matches(userData.pseudo, /^[a-z0-9éèàùûêâôë][a-z0-9éèàùûêâôë '-]+$/i)) {return res.status(400).json({message: " Pseudo doit être en lettre ou chiffre"})}
+
+    if (userData.fonction.length > 0 &&  (!validator.matches(userData.fonction, /^[a-zéèàùûêâôë][a-zéèàùûêâôë '-]+$/i)) ) {return res.status(400).json({message: " veuillez ne saisir que des lettres"}) }   
+    
+        // Valider password avec password-validator
+    if(!schema.validate(userData.password)) {return res.status(400).json({message: " Mot de passe doit avoir 8 et 20 caractères, 1 majuscule, 1 minuscule, 1 caractère spécial"})}
+    
+    // Vérifier si password et passwordCheck soit le même
+    if ( userData.password !== userData.passwordCheck) { return res.status(400).json({message: "Mot de passe doit être le même pour les 2 champs"})}
+
+        // Après validation des données, chercher si email est déjà utilisé ; si non crée user
+    else { db.Users.findOne ( {  where: { email: emailHash }})
+        .then( user => { 
+                // Si trouvé user dans BDD avec email => email déjà utilisé
+            if( user) {
+              return  res.status(400).json({message: " email déjà utilisé"}); 
+            }
+            else {  // Email n'est pas dans BDD
+                // Vérifier si pseudo est déjà présente dans BDD 
+                db.Users.findOne ( { where: { pseudo : userData.pseudo}})
+                .then( user => { 
+                    if (user) {     // Pseudo trouvé dans BDD
+                       return res.status(400).json({ message: " pseudo deja utilisé"});
+                         
+                    }
+                    else {          // Pas de pseudo dans BDD
+                        
+                        bcrypt.hash(userData.password,10)   // Hash password, puis créer user
+                            .then( hash => {
+                                // S'il n'y a pas photo, prendre nom de l'image avatar default,
+                                // Si non prendre le nom de requete file
+                                let avatarName = "";
+                                if ( req.file) { avatarName = req.file.filename}
+                                else { avatarName = "avatar_default.png"} 
+
+                                    // Créer user
+                                const newUser = db.Users.create({
+                                    email: emailHash,
+                                    nom: userData.nom,
+                                    prenom: userData.prenom,
+                                    password: hash,
+                                    fonction: userData.fonction,
+                                    pseudo: userData.pseudo,
+                                    isAdmin: 0, 
+                                    avatar: avatarName
+                                });
+                                res.status(201).json( { message: "Utilisateur crée avec succès"})
+                            })
+                            .catch( () => { 
+                                    res.status(400).json( {messsage: " Problème pour crée utilisateur" });   
+                            } )
+                    }
+                })  
+            }
+        })
+        .catch( () => res.status(500).json( { message: "Pb de server, impossible chercher email user"}))       
+    }
+})
