561cdacf3ee92832a5150358d983245ca1423ac9
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:28:52 +0200

    import module server app.js

---
+++ backend/app.js
@@ -0,0 +1,7 @@
+// Import les modules nécessaires pour serveur
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+const cookieParser = require('cookie-parser')
+const app = express();
+
