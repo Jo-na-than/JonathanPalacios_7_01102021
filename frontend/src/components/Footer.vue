a470af71764c89f300f24a1104f8696a49959d1a
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-12 00:41:58 +0200

    footer.vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer>
+        <a class="blue mt-5 mb-3" href="">© 2021 Groupomania</a>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+    .blue {
+        color: #0000FF !important
+    }
+    .blue:hover,
+    .blue:focus {
+        color:  black !important;
+        opacity: 0.5;
+        background: white;
+    }
+</style>>
+
+
