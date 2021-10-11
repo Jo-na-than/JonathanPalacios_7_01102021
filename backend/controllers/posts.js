92b175adf21ae71cdf367e679737a40d0863e89d
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:41:20 +0200

    création post controllers/posts.js

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -7,3 +7,27 @@ const Sequelize = require('sequelize');
 const association = require('../models/association').association
 const sequelize = require('../models/index').sequelize;
 const models = association(sequelize);
+
+// ===> Route pour créer 1 post <===
+exports.createPost = (req, res) => {
+    let file_url = "";
+    
+        // S'il y a req file, enregistrer son nom dans BDD; 
+        if (req.file) {
+            file_url = req.file.filename
+        }
+        else { file_url = ""};
+
+            // Enregistrer dans table Posts
+        const post = db.Posts.create({
+            content: xss(req.body.content),
+            img_url: file_url,
+            userId: req.body.userId
+            })
+            .then( () => res.status(201).json("Votre article a été enregistré"))
+            .catch( err => {
+                console.log(err);
+                res.status(500).json("Créer post erreur")
+            })
+    
+}
