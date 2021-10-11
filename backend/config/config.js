181bdddbca906dcf14d2fec9a61ec28504ff81ee
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 21:09:58 +0200

    config

---
+++ backend/config/config.js
@@ -0,0 +1,20 @@
+// fichier config pour connection Sequelize 
+const fs = require('fs');
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.NAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+   
+  },
+  test: {
+    
+  },
+  production: {
+   
+  }
+};
