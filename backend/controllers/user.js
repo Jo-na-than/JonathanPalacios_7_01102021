d5108ae5b3c35abf8c7cc3f038f513ec4285b790
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:49:58 +0200

    envoi mail si reset/update password + schema pour valider password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,3 +10,20 @@ const Sequelize = require('sequelize');
 const association = require('../models/association').association
 const sequelize = require('../models/index').sequelize;
 const models = association(sequelize);
+
+// Middleware pour envoi mail si reset/update password
+const sendEmail = require('./email');
+
+// Schema pour valider password
+const validator = require('validator')
+const passwordValidator = require("password-validator");
+const schema = new passwordValidator();
+schema
+    .is().min(8)                                    // Longueur minimum 8
+    .is().max(20)                                   // Longueur maximum 100
+    .has().uppercase()                              // Doit contenir une majuscule
+    .has().lowercase()                              // Doit contenir une minuscule
+    .has().digits(1)                                // Doit contenir au moins 1 chiffres
+    .has().not().spaces()                           // Doit contenir aucun espace
+    .is().not().oneOf(["Passw0rd", "Password123"])  // Mot de passes blacklistés
+    .has(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&.])[A-Za-z\d@$!%*#?&.]{8,}$/)  // regex pour password fort
