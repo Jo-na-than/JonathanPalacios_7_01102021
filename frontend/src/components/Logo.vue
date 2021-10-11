39130f5ac5e7af6558bb194a3e12f49af14abae3
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-12 00:45:59 +0200

    logo.vue

---
+++ frontend/src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div >
+        <img class = "" src="../assets/images/icon-left-font-monochrome-black.svg" alt="logo Groupomania"/>
+    </div>
+</template>
+    
+<script>
+export default {
+    name: "Logo",
+    props :{}
+}
+</script>
+
+<style scoped>
+img{
+    width: 12rem;
+    background: none;
+}
+</style>
