f6382984ef80aa052323e82c289d5b20f0257228
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:29:37 +0200

    import module protection api app.js

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser')
 const app = express();
 
+// Import les modules pour une protection api
+const cors = require("cors");
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit'); 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100  // limit each IP to 100 requests per windowMs
+});
+const expressSanitizer = require('express-sanitizer');
