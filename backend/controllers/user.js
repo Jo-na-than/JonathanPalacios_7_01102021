76a4b38381a34277742920e08e5280045fe107c0
Jo-na-than <jonathan.palacios@orange.fr>
2021-10-11 20:49:05 +0200

    model sequelize controllers/user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,3 +3,10 @@ const crypto = require ('crypto')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+
+// Model Sequelize
+const db = require('../models')
+const Sequelize = require('sequelize');
+const association = require('../models/association').association
+const sequelize = require('../models/index').sequelize;
+const models = association(sequelize);
